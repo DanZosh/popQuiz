8f147488d2ab4f0bb925846ef1db1fa74edcbc92
Danny Maier <dannymaier@gmail.com>
2020-10-30 21:08:51 -0700

    I got the test HTML and test JS to work in the question cycling. I'm goign to attempt to merge with index and script files since this was the hardest part and its working better

--- test.js
+++ test.js
@@ -6,28 +6,45 @@ console.log(goButtonEl)
 console.log(questionEl) 
 console.log(answerListEl)
  
-var questions = [
-     Q1={
-        "Query":"Commonly used data types DO NOT include as:",
-        "Answers" :["strings", "booleans", "alerts", "numbers"],
-        "correctAnswer": 3
-     },
-    
-
-    //  Q2={
-    //     "Query":"The condition in an if / else statement is enclosed within _____.",
-    //     "Answers" :["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
-    //     "correctAnswer": 3,
-    //  }
+var questionsObject = [
+    Q1={
+        "Query":"Q1: Commonly used data types DO NOT include as:",
+        "Answers" :["1. strings", "2. booleans", "3. alerts", "4 .numbers"],
+        "correctAnswer": 2
+    },
+    Q2={ 
+        "Query":"Q2: The condition in an if / else statement is enclosed within _____.",
+        "Answers" :["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        "correctAnswer": 2,
+    },
+    Q3={
+        "Query":"Q3: Arrays in JavaScript can be used to store ____.",
+        "Answers" :["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        "correctAnswer": 3,
+    },
+    Q4={
+        "Query":"Q4:String values must be enclosed within ____ when being assigned to variables.",
+        "Answers" :["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+        "correctAnswer": 2,
+    },
+    Q5={
+        "Query":"Q5: Arrays in JavaScript can be used to store ____.",
+        "Answers" :["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        "correctAnswer": 3,
+    }
     ]
+var questionCounter=0
+console.log(questionCounter)
 
-    var Question1 = questions[0]["Query"]
-console.log(Question1)
-//returns question  
-    var Answers1 = questions[0]["Answers"]
-    console.log(Answers1)
-    //returns Booleans
-    var Booleans = questions[0]["Answers"][1]
+    var Question = questionsObject[questionCounter]["Query"]
+    console.log(Question)
+    //returns question  
+    var Answers = questionsObject[questionCounter]["Answers"]
+    // console.log(Answers)
+    //returns Answers
+
+    //trash
+    var Booleans = questionsObject[questionCounter]["Answers"][1]
     console.log(Booleans)
     //returns Booleans
 
@@ -40,16 +57,74 @@ goButtonEl.addEventListener("click", function(event) {
     console.log(event.target);
 
     // replace the innerHTML of `questionEl` with Question1
-    console.log(Question1);
-    questionEl.innerHTML= Question1;
+    console.log(Question);
+    questionEl.innerHTML= Question
 
 
-    for (var i = 0; i < Answers1.length; i++) {
+    for (var i = 0; i < Answers.length; i++) {
+
+        var questionButton = document.createElement("button");
+        // console.log(questionButton)
+        // this creates a button element
+        var questionText = document.createTextNode(Answers[i]);
+        // console.log(questionText)
+        // this creates text
+        questionButton.appendChild(questionText)
+        // console.log(questionButton)
+        // this appends the text to the element
+        answerListEl.appendChild(questionButton)
+}
+
+
+questionCounter++
+console.log(questionCounter)
+});
+
+
+
+answerListEl.addEventListener("click", function(event) {
+
+
+    event.preventDefault();
+    console.log(event.target);
+    event.stopPropagation();
+
+    //WHEN i click on an answer
+    //THEN the `questionEl` is replaced with  questionsObject[1]["Query"]
+    console.log(questionCounter)
+    console.log(questionsObject[1]["Query"])
+    console.log(questionsObject[questionCounter]["Query"])
+    //why isn't `question` updating
+    // console.log(Question)
+    
+    //This resets to question 2
+    questionEl.innerHTML= questionsObject[questionCounter]["Query"]
+
+    console.log(answerListEl)
+    console.log(answerListEl.childNodes)
+    
+    // The below works 
+    answerListEl.removeChild(answerListEl.childNodes[0])
+    answerListEl.removeChild(answerListEl.childNodes[0])
+    answerListEl.removeChild(answerListEl.childNodes[0])
+    answerListEl.removeChild(answerListEl.childNodes[0])
+
+    // // the below doesn't work
+    // for (let i = 0; i < answerListEl.childNodes.length; i++) {
+    //     answerListEl.removeChild(answerListEl.childNodes[0]) 
+    // }
+
+    // // the below doesn't work
+    // while (answerListEl.childNodes.length >0){
+    //     answerListEl.removeChild(answerListEl.childNodes[-1])
+    //     }
+
+    for (var i = 0; i < Answers.length; i++) {
 
         var questionButton = document.createElement("button");
         console.log(questionButton)
         // this creates a button element
-        var questionText = document.createTextNode(Answers1[i]);
+        var questionText = document.createTextNode(questionsObject[questionCounter]["Answers"][i]);
         console.log(questionText)
         // this creates text
         questionButton.appendChild(questionText)
@@ -58,7 +133,23 @@ goButtonEl.addEventListener("click", function(event) {
         answerListEl.appendChild(questionButton)
 }
 
+
+questionCounter++
+
+console.log(questionCounter)
 });
+
+
+
+
+console.log(questionCounter)
+
+
+
+
+
+
+
     //   if(event.target.matches("button")) {
         // if the click happening to a button, this other logic happens below
         //event.target is the item we clicked
